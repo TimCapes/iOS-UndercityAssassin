edd49c946d01c331f95d47c10b85d3e5415293a6
Tim Capes <capestim@gmail.com>
2013-07-28 20:10:07 -0400

    Stubbed out options menu cells

--- Undercity Assassin/Undercity Assassin/SourceCode/GameOptions/UAGameOptionsViewController.m
+++ Undercity Assassin/Undercity Assassin/SourceCode/GameOptions/UAGameOptionsViewController.m
@@ -10,6 +10,8 @@
 
 @interface UAGameOptionsViewController ()
 
+@property (weak, nonatomic) IBOutlet UITableView *gameOptionsView;
+
 @end
 
 @implementation UAGameOptionsViewController
@@ -35,4 +37,58 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)viewDidUnload {
+    [self setGameOptionsView:nil];
+    [super viewDidUnload];
+}
+#pragma mark TableView Methods
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 3;
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    if (section==0) {
+        return @"User";
+    } else if (section==1){
+        return @"Scores";
+    } else {
+        return @"Options";
+    }
+}
+- (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)section {
+    if (section==0) {
+        return 1;
+    } else if (section==1){
+        return 2;
+    } else {
+        return 1;
+    }
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section==0) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UAGamePlayerCell"];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UAGamePlayerCell"];
+        }
+        cell.textLabel.text = @"Facebook Player Cell";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        return cell;
+    } else if (indexPath.section==1) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UAGamePlayerCell"];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UAGamePlayerCell"];
+        }
+        cell.textLabel.text = @"Scoring Cells";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        return cell;
+    } else { //if(indexPath.section==2) {
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UAGamePlayerCell"];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UAGamePlayerCell"];
+        }
+        cell.textLabel.text = @"Options Cells";
+        cell.textLabel.font = [UIFont systemFontOfSize:15];
+        return cell;
+    }
+}
 @end
